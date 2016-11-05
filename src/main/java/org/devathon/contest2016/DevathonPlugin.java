6b2959732e25fff79fa82bf0299399b2f61fad57
MiniDigger <admin@minidigger.me>
2016-11-05 14:28:31 +0100

    fuck your sh script, windows master race!

--- src/main/java/org/devathon/contest2016/DevathonPlugin.java
+++ src/main/java/org/devathon/contest2016/DevathonPlugin.java
@@ -7,11 +7,13 @@ public class DevathonPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         // put your enable code here
+        getLogger().info("test");
     }
 
     @Override
     public void onDisable() {
         // put your disable code here
+        getLogger().info("test2");
     }
 }
 
